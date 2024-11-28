3f006f8fb87bba0fd218b4995ab84b5c75a704f6
00016180wiut <00016180wiut@gmail.com>
2024-11-28 20:10:01 +0500

    Created DB, installed dependencies

---
+++ WAD.BACKEND.00016180/WAD.BACKEND.00016180/Data/TaskTrackerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WAD.BACKEND._00016180.Models;
+
+namespace WAD.BACKEND._00016180.Data
+{
+    public class TaskTrackerDbContext: DbContext
+    {
+        public TaskTrackerDbContext(DbContextOptions<TaskTrackerDbContext> options) : base(options) { }
+
+        public DbSet<Models.Task> Task { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- WAD.BACKEND.00016180/WAD.BACKEND.00016180/obj/Debug/net6.0/WAD.BACKEND.00016180.AssemblyInfo.cs
+++ WAD.BACKEND.00016180/WAD.BACKEND.00016180/obj/Debug/net6.0/WAD.BACKEND.00016180.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WAD.BACKEND.00016180")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87e797582b93f6f364c9fa6a4b1f0bedcd8fe9f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12f9754e4c5d16ce5b5414819e35e27762ee3c95")]
 [assembly: System.Reflection.AssemblyProductAttribute("WAD.BACKEND.00016180")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WAD.BACKEND.00016180")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
