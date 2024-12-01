c7c13ea2fb9d4d0484c5afd72cc8af345bfb9fc3
00016180 <0001618wiut@gmail.com>
2024-12-02 00:07:16 +0500

    created other components and implemented routing

--- WAD.FRONTEND.00016180/src/app/components/home/home.component.ts
+++ WAD.FRONTEND.00016180/src/app/components/home/home.component.ts
@@ -3,6 +3,7 @@ import { Task } from '../../types';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { ServiceTaskTrackerService } from '../../service-task-tracker.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -13,6 +14,7 @@ import { ServiceTaskTrackerService } from '../../service-task-tracker.service';
 })
 export class HomeComponent {
   TaskTrackerService = inject(ServiceTaskTrackerService);
+  router = inject(Router);
   tasks: Task[] = [];
   ngOnInit() {
     this.TaskTrackerService.getAllTasks().subscribe((result) => {
@@ -32,5 +34,21 @@ export class HomeComponent {
   formatDate(date: string | Date): string {
     const parsedDate = typeof date === 'string' ? new Date(date) : date;
     return parsedDate.toDateString();
+  }
+  EditClicked(taskId: number) {
+    console.log(taskId, 'Edit');
+    this.router.navigateByUrl('/edit/' + taskId);
+  }
+  DetailsClicked(taskId: number) {
+    console.log(taskId, 'Details');
+    this.router.navigateByUrl('/details/' + taskId);
+  }
+  DeleteClicked(taskId: number) {
+    console.log(taskId, 'Delete');
+    this.router.navigateByUrl('/delete/' + taskId);
+  }
+  CreateClicked() {
+    console.log('Create');
+    this.router.navigateByUrl('/create');
   }
 }
--- WAD.FRONTEND.00016180/src/app/service-task-tracker.service.ts
+++ WAD.FRONTEND.00016180/src/app/service-task-tracker.service.ts
@@ -3,13 +3,21 @@ import { inject, Injectable } from '@angular/core';
 import { Task } from './types';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ServiceTaskTrackerService {
   httpClient = inject(HttpClient);
-  constructor() { }
+  constructor() {}
 
   getAllTasks() {
-    return this.httpClient.get<Task[]>('https://localhost:5169/api/Task/GetAll');
+    return this.httpClient.get<Task[]>(
+      'https://localhost:5169/api/Task/GetAll'
+    );
+  }
+
+  getById(taskId: number) {
+    return this.httpClient.get<Task>(
+      `https://localhost:5169/api/Task/GetById/${taskId}`
+    );
   }
 }
