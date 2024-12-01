7caaa748d9efee126f90e827f4a7170e5d851918
00016180 <0001618wiut@gmail.com>
2024-12-02 01:33:50 +0500

    created navigation bar

--- WAD.FRONTEND.00016180/src/app/app.component.ts
+++ WAD.FRONTEND.00016180/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ WAD.FRONTEND.00016180/src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.css',
+})
+export class NavigationComponent {
+  router = inject(Router);
+  CreateClicked() {
+    this.router.navigateByUrl('create');
+  }
+  HomeIconClicked() {
+    this.router.navigateByUrl('home');
+  }
+}
