e1f02b41d6f6fede548b0e14e46feb4b90788e5e
00016180 <0001618wiut@gmail.com>
2024-12-02 11:46:06 +0500

    Created create functionality

--- WAD.FRONTEND.00016180/src/app/components/create/create.component.ts
+++ WAD.FRONTEND.00016180/src/app/components/create/create.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ServiceTaskTrackerService } from '../../service-task-tracker.service';
+import { Router } from '@angular/router';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { Category } from '../../types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
   templateUrl: './create.component.html',
-  styleUrl: './create.component.css'
+  styleUrl: './create.component.css',
 })
 export class CreateComponent {
+  TaskTrackerService = inject(ServiceTaskTrackerService);
+  router = inject(Router);
+  categories: any = [];
+  categoryId: number = 0;
+  createTask: any = {
+    title: '',
+    notes: '',
+    categoryId: 0,
+    dueDate: '',
+    enteredDate: new Date(),
+    isCompleted: false,
+  };
 
+  ngOnInit() {
+    this.TaskTrackerService.getAllCategories().subscribe(
+      (result) => (this.categories = result)
+    );
+  }
+
+  create() {
+    this.createTask.categoryId = this.categoryId;
+    this.TaskTrackerService.create(this.createTask).subscribe((result) => {
+      alert('Task saved');
+      this.router.navigateByUrl('home');
+    });
+  }
 }
--- WAD.FRONTEND.00016180/src/app/service-task-tracker.service.ts
+++ WAD.FRONTEND.00016180/src/app/service-task-tracker.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Task } from './types';
+import { Category, Task } from './types';
 
 @Injectable({
   providedIn: 'root',
@@ -19,5 +19,25 @@ export class ServiceTaskTrackerService {
     return this.httpClient.get<Task>(
       `https://localhost:5169/api/Task/GetById/${taskId}`
     );
+  }
+
+  edit(task: Task) {
+    return this.httpClient.put('https://localhost:5169/api/Task/Update', task);
+  }
+
+  delete(id: number) {
+    return this.httpClient.delete(
+      `https://localhost:5169/api/Task/Delete/${id}`
+    );
+  }
+  create(task: Task) {
+    return this.httpClient.post<Task>(
+      'https://localhost:5169/api/Task/Create',
+      task
+    );
+  }
+
+  getAllCategories() {
+    return this.httpClient.get('https://localhost:5169/api/Category/GetAll');
   }
 }
