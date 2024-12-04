ccf7bdd44b301c9eeb0d059235fdedc70dca2a48
00016180 <00016180wiut@gmail.com>
2024-12-04 18:40:00 +0500

    deleted migrations

--- WAD.BACKEND.00016180/WAD.BACKEND.00016180/obj/Debug/net6.0/WAD.BACKEND.00016180.AssemblyInfo.cs
+++ WAD.BACKEND.00016180/WAD.BACKEND.00016180/obj/Debug/net6.0/WAD.BACKEND.00016180.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WAD.BACKEND.00016180")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e25d3ea07e14658a43113c68ee394d3baafe1c28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0099b6247d31d65a99d82232e336b2fb41502b50")]
 [assembly: System.Reflection.AssemblyProductAttribute("WAD.BACKEND.00016180")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WAD.BACKEND.00016180")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
